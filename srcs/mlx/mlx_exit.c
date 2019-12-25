5377ed0afbc40f7a5ec158603bdbe80be8470e21
Mbos <melodiebos@gmail.com>
2019-12-25 15:21:28 +0100

    exit_function

--- srcs/mlx/mlx_exit.c
+++ srcs/mlx/mlx_exit.c
@@ -6,15 +6,22 @@
 /*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/25 11:24:08 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/25 12:25:05 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/25 15:18:28 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../../includes/cube3d.h"
 
-int exit_propre(void *mlx)
+int mlx_exit(void *mlx)
 {
     (void)mlx;
     exit(0);
+}
+
+t_bool	mlx_display_exit(t_mlx mlx)
+{
+	if (!(mlx_hook(mlx.win, 17, 0, &mlx_exit, &mlx)))
+		return (false_ret(__func__));
+	return (True);
 }
