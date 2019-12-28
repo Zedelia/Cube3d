714298c65763699b58d0aa8f688c1e9490b8e89c
Zedelia <melodiebos@gmail.com>
2019-12-28 16:00:07 +0100

    organize_for_check_tests

--- srcs/main.c
+++ srcs/main.c
@@ -1,16 +1,40 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/28 15:28:30 by mbos         #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/28 15:42:40 by mbos        ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
 #include <string.h>
 #include "../includes/cube3d.h"
 
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	(void)argc;
-	// (void)argv;
+	int ret;
 	t_map *map;
+	int i = 1;
 
-	map_init(&map, argv[1]);
-	map_printf(map);
+	argc += 1;
 
-	return (0)
-;
+	while (i < argc)
+	{
+		ret = map_init(&map, argv[i]);
+		map_printf(map);
+		if (ret == 0)
+			ft_printf(RED"ok: %s\n"RESET, argv[i]);
+		if (ret != 0)
+		{
+			ft_printf(MAGENTA"Error: map shouldn't be validated\n"RESET);
+			return (0);
+		}
+		i++;
+	}
+	return (0);
 }
