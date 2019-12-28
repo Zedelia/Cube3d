fb58f1604e2d6473e1be6c39f90be93183a27c1e
Zedelia <melodiebos@gmail.com>
2019-12-28 17:20:10 +0100

    update_return_func

--- srcs/utils/return_func.c
+++ srcs/utils/return_func.c
@@ -6,7 +6,7 @@
 /*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/27 18:11:41 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/27 18:57:44 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/28 16:56:33 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,8 +15,9 @@
 
 t_bool	return_false(const char *namefunc, char const *error_msg)
 {
+	ft_printf(RED"");
 	perror(namefunc);
 	if (error_msg)
-		ft_printf(RED"Error\n%s\n"RESET, error_msg);
+		ft_printf(RED"Error"RESET"\n-> %s\n\n", error_msg);
 	return (False);;
 }
