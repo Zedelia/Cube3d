d5bef78103e5b45f460c180a974f438604dc5b29
Zedelia <melodiebos@gmail.com>
2019-12-28 16:21:41 +0100

    tests

--- srcs/main.c
+++ srcs/main.c
@@ -1,40 +1,18 @@
-/* ************************************************************************** */
-/*                                                          LE - /            */
-/*                                                              /             */
-/*   main.c                                           .::    .:/ .      .::   */
-/*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
-/*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/12/28 15:28:30 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/28 15:42:40 by mbos        ###    #+. /#+    ###.fr     */
-/*                                                         /                  */
-/*                                                        /                   */
-/* ************************************************************************** */
-
+#include <stdio.h>
 #include <string.h>
 #include "../includes/cube3d.h"
 
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int ret;
+	(void)argc;
+	// (void)argv;
 	t_map *map;
-	int i = 1;
+	int ret;
 
-	argc += 1;
+	ret = map_init(&map, argv[1]);
+	ft_printf("RETURN MAP INIT = %d\n", ret);
+	map_printf(map);
 
-	while (i < argc)
-	{
-		ret = map_init(&map, argv[i]);
-		map_printf(map);
-		if (ret == 0)
-			ft_printf(RED"ok: %s\n"RESET, argv[i]);
-		if (ret != 0)
-		{
-			ft_printf(MAGENTA"Error: map shouldn't be validated\n"RESET);
-			return (0);
-		}
-		i++;
-	}
-	return (0);
+	return (0)
+;
 }
