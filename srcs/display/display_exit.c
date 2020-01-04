c289f8c5d1783467c7d9675c581ed16377c7ad4a
Mbos <melodiebos@gmail.com>
2020-01-04 14:36:39 +0100

    minor_changes

--- srcs/display/display_exit.c
+++ srcs/display/display_exit.c
@@ -6,7 +6,7 @@
 /*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/25 11:24:08 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/27 18:46:56 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/04 11:37:12 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -22,6 +22,6 @@ static int display_exit(void *mlx)
 t_bool	display_exit_on_click(t_mlx *mlx)
 {
 	if (!(mlx_hook(mlx->win, 17, 0, &display_exit, &mlx)))
-		return (return_false(__func__, "[FAIL] init exit"));
+		return (return_false(__func__, "[FAIL] exit"));
 	return (True);
 }
--- srcs/rays/rays_setup.c
+++ srcs/rays/rays_setup.c
@@ -6,7 +6,7 @@
 /*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/26 14:24:41 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/26 14:24:46 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/04 14:32:32 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,11 +16,11 @@
 t_rays	ray_setup(double angle)
 {
 	t_rays	ray;
-	double	radian_a;
+	double	angle_radian;
 
-	radian_a = degrees_to_radian(angle);
+	angle_radian = degrees_to_radian(angle);
 	ray = rays_init();
-	ray.x = -sin(radian_a);
-	ray.y = cos(radian_a);
+	ray.x = -sin(angle_radian);
+	ray.y = cos(angle_radian);
 	return (ray);
 }
