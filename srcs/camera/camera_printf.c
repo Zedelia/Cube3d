41e8c3980e70c25ff0ba85ed0a2412bab16f4b7c
Mbos <melodiebos@gmail.com>
2020-01-06 17:18:57 +0100

    update_camera_show

--- srcs/camera/camera_printf.c
+++ srcs/camera/camera_printf.c
@@ -6,7 +6,7 @@
 /*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/04 13:04:12 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/04 13:56:55 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/06 17:17:53 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,7 +17,8 @@ void	camera_printf(t_camera *cam)
 {
 	ft_printf("pos x: %d\n", (int)cam->pos_x);
 	ft_printf("pos y: %d\n", (int)cam->pos_y);
-	ft_printf("direc x: %d\n", (int)cam->direction.x);
-	ft_printf("direc y: %d\n", (int)cam->direction.y);
+	printf("direc x: %f\n", cam->direction.x);
+	printf("direc y: %f\n", cam->direction.y);
 	ft_printf("rotation angle: %d\n", (int)cam->rotation_angle);
+
 }
