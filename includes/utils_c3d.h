8209e197d6f05d3f4e8d4eb7335ba8d63a040f77
Mbos <melodiebos@gmail.com>
2020-01-20 13:29:06 +0100

    clean_wall_textured_display

--- includes/utils_c3d.h
+++ includes/utils_c3d.h
@@ -6,7 +6,7 @@
 /*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/26 13:08:50 by melodiebos   #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/20 12:57:11 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/20 13:10:35 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -30,6 +30,6 @@ void		ft_draw_pix(t_mlx *mlx, t_vect v, int size_obj);
 double		ray_math_dist(t_vect hit, t_mlx *mlx);
 double		dist_correct_fish_eye(t_rays r, t_mlx *mlx, double dist);
 int			ft_pixel_get_color(t_img img, int x, int y);
-t_bool	ft_check_incorrect_input_text(char a, char b, char *line);
+t_bool		ft_check_incorrect_input_text(char a, char b, char *line);
 
 #endif
--- srcs/display/display_textures.c
+++ srcs/display/display_textures.c
@@ -6,58 +6,52 @@
 /*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/18 15:56:48 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/20 13:06:05 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/20 13:27:20 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../../includes/cube3d.h"
 
-t_bool	draw_column_hz_hit(t_rays r, t_mlx *mlx, t_img img, int x)
+t_bool	draw_wall_hz_hit(t_rays r, t_map *map, t_img img, t_mlx *mlx, int x)
 {
 	int y;
-	double wall_end;
-	double wall_start;
+	double bottom;
+	double top;
 	double offset_x;
 	double	offset_y;
 
-	offset_x = (r.wall.hz_hit.x - (int)r.wall.hz_hit.x) * mlx->map->north.width;
-	offset_x = offset_x < 0 ? -offset_x : offset_x;
-	wall_start = mlx->map->r_height / 2 - mlx->map->r_height / r.distance / 2;;
-	wall_end = mlx->map->r_height / 2 + mlx->map->r_height / r.distance / 2;
-	y = wall_start;
-	while (y < wall_end)
+	offset_x = (r.wall.hz_hit.x - (int)r.wall.hz_hit.x) * map->north.width;
+	offset_x = ft_abs(offset_x);
+	top = map->r_height / 2 - map->r_height / r.distance / 2;;
+	bottom = map->r_height / 2 + map->r_height / r.distance / 2;
+	y = top;
+	while (y < bottom)
 	{
-		offset_y = (y - wall_start) * ((double)mlx->map->north.height / (wall_end - wall_start));
+		offset_y = (y - top) * ((double)map->north.height / (bottom - top));
 		ft_pixel_put(mlx, x, y, ft_pixel_get_color(img, offset_x, offset_y));
 		y++;
 	}
 	return (True);
 }
 
-t_bool	 draw_column_vt_hit(t_rays r, t_mlx *mlx, t_img img, int x)
+t_bool	 draw_wall_vt_hit(t_rays r, t_map *map, t_img img, t_mlx *mlx, int x)
 {
 	int y;
-	double	color;
-	double	wall_end;
-	double	wall_start;
+	double	bottom;
+	double	top;
 	double 	offset_x;
 	double	offset_y;
 
-	color = 0x0;
-	offset_x = (r.wall.vt_hit.y - (int)r.wall.hz_hit.y) * mlx->map->east.width;
-	offset_x = offset_x < 0 ? -offset_x : offset_x;
-	while (offset_x > mlx->map->east.height)
+	offset_x = (r.wall.vt_hit.y - (int)r.wall.hz_hit.y) * map->east.width;
+	while ((offset_x = ft_abs(offset_x)) > map->east.height)
+		offset_x = map->east.height - offset_x;
+	top = map->r_height / 2 - map->r_height / r.distance / 2;;
+	bottom = map->r_height / 2 + map->r_height / r.distance / 2;
+	y = top;
+	while (y < bottom)
 	{
-		offset_x = mlx->map->east.height - offset_x;
-		offset_x = offset_x < 0 ? -offset_x : offset_x;
-	}
-	wall_start = mlx->map->r_height / 2 - mlx->map->r_height / r.distance / 2;;
-	wall_end = mlx->map->r_height / 2 + mlx->map->r_height / r.distance / 2;
-	y = wall_start;
-	while (y < wall_end)
-	{
-		offset_y = (y - wall_start) * ((double)mlx->map->west.height / (wall_end - wall_start));
+		offset_y = (y - top) * ((double)map->west.height / (bottom - top));
 		ft_pixel_put(mlx, x, y,ft_pixel_get_color(img, offset_x, offset_y));
 		y++;
 	}
@@ -74,13 +68,13 @@ t_bool	 display_textured_walls(t_mlx *mlx)
 	{
 		temp = mlx->cam.ray_tab[x];
 		if (temp.facing_up && temp.wall.hz)
-			draw_column_hz_hit(temp, mlx, mlx->map->north, x);
+			draw_wall_hz_hit(temp, mlx->map, mlx->map->north, mlx, x);
 		else if (temp.facing_down && temp.wall.hz)
-			draw_column_hz_hit(temp, mlx, mlx->map->south, x);
+			draw_wall_hz_hit(temp, mlx->map, mlx->map->south, mlx, x);
 		else if (temp.facing_left && temp.wall.vt)
-			draw_column_vt_hit(temp, mlx, mlx->map->west, x);
+			draw_wall_vt_hit(temp, mlx->map, mlx->map->west, mlx, x);
 		else if (temp.facing_right && temp.wall.vt)
-			draw_column_vt_hit(temp, mlx, mlx->map->east, x);
+			draw_wall_vt_hit(temp, mlx->map, mlx->map->east, mlx, x);
 		x++;
 	}
 	return (True);
